545d173e2113fc8588e9bdf630213dfec877d6d9
Estudiante <estudiante@iMac-de-usuario.local>
2017-07-18 18:55:48 +0100

    SignIn

--- Podfile
+++ Podfile
@@ -5,6 +5,9 @@ target 'iSwim' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'SwipeCellKit', '~> 1.9'
+  pod 'Firebase/Core'
+  pod 'Firebase/Database’
+  pod 'Firebase/Auth'
 
   # Pods for iSwim
 
--- iSwim/Sign.swift
+++ iSwim/Sign.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class Sign : UIViewController {
     
     @IBOutlet var sign : UIButton?
     @IBOutlet var register : UIButton?
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
     
+    @IBAction func loginAction(_ sender: UIButton) {
+        if emailTextfield.text != "" && passwordTextfield.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!, completion: { (user, error) in
+                if user != nil {
+                    //Sign in successful
+                } else {
+                    if let myError = error {
+                        print (myError)
+                    } else {
+                        print ("ERROR")
+                    }
+                }
+            })
+        }
+    }
     
+    @IBAction func signupAction(_ sender: UIButton) {
+        if emailTextfield.text != "" && passwordTextfield.text != "" {
+            
+            Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!, completion: { (user, error) in
+                if user != nil {
+                    //Sign Up successful
+                } else {
+                    if let myError = error {
+                        print (myError)
+                    } else {
+                        print ("ERROR")
+                    }
+                }
+            })
+        }
+    }
     override func viewDidLoad() {
         
         super.viewDidLoad()
